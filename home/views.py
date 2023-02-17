04d4a54c3b83f82f6c51c7c68c619ef5587d6982
Mehedi Hasan <mehedi.mgt2019@gmail.com>
2023-02-17 16:28:04 +0600

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    context = {
+        "variable1":"Munna is great",
+        "variable2":"Mehedi is my first name"
+    }
+    return render(request, 'index.html', context)
+    #return HttpResponse("this is homepage")
+
+def about(request):
+    return render(request, 'about.html')
+  
+
+def memories(request):
+    return render(request, 'memories.html')
+  
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date= datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
+
+
+def journey(request):
+    return render(request, 'journey.html')
+    
